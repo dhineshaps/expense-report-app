d67e157669a82d987b8cd92bffb2305a6381a4c0
Dhinesh Palanisamy <84257275+dhineshaps@users.noreply.github.com>
2025-05-08 14:30:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config.get('preauthorized', {})
+    config['cookie']['expiry_days'])
 )
 
 # --- Title and Login ---
